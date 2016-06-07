01f5277f92d951d3a9af3cb8ef9c2b5153d2ecaf
alonikomax <aloniko.zus@gmail.com>
2016-06-07 22:34:17 +0300

    Deleted unneeded function

--- app/app.component.ts
+++ app/app.component.ts
@@ -10,17 +10,5 @@ import {Component} from "@angular/core";
 `,
 })
 export class AppComponent {
-    public counter: number = 16;
-
-    public get message(): string {
-        if (this.counter > 0) {
-            return this.counter + " taps left";
-        } else {
-            return "Hoorraaay! \nYou are ready to start building!";
-        }
-    }
-
-    public onTap() {
-        this.counter--;
-    }
+    
 }
